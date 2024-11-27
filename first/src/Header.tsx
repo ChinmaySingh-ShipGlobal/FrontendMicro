2a965ec474f329b8e280d980b30918fc969f839f
Chinmay <chinmay.singh@shipglobal.in>
2024-11-27 15:33:59 +0530

    tried input component from remote to host

--- first/src/Header.tsx
+++ first/src/Header.tsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return <div>This is Header</div>;
+  return (
+    <div className="bg bg-yellow-100 w-full">
+      <input
+        type="text"
+        className="bg bg-white w-full"
+        onChange={(e) => console.log(`I am changing by ${e.target.value} `)}
+      />
+    </div>
+  );
 }
--- host/src/App.tsx
+++ host/src/App.tsx
@@ -5,31 +5,12 @@ import "./App.css";
 const Header = lazy(() => import("first/Header"));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      I am host and below is remote
+      <div className="bg bg-red-600 p-4 flex items-center justify-center">
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Header />
     </>
   );
 }
